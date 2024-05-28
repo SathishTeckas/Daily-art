2c48d648e536036f8de9d9fb6ca42d1e28b919c5
sathish <sathish@kalakendradao.org>
2024-05-28 12:30:31 +0530

    Updated

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { countVotesAndUpdateBattle } from '../../utils/countVotesAndBattles';
+export default async function handler(req, res) {
     console.log("fsdfdsfsdf");
+    await countVotesAndUpdateBattle();
     res.status(200).end('Hello Cron!');
   }
--- pages/api/mintNfts.ts
+++ pages/api/mintNfts.ts
@@ -1,11 +1,15 @@
-import { NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { countVotesAndUpdateBattle } from '../../utils/countVotesAndBattles';
-export async function GET() {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
     try {
       console.log("called");
         await countVotesAndUpdateBattle();
-        return NextResponse.json({ ok: true });
+      res.status(200).json({ success: true });
     } catch (error) {
-      return NextResponse.json({ ok: false });
+      res.status(500).json({ success: false, error: 'Error' });
     }
+  } else {
+    res.status(405).json({ success: false, message: 'Method Not Allowed' });
+  }
 }
